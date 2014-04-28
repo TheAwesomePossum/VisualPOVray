2e35866f6f816fd32daae6edc243310c1b880f09
westernState <bendix.cunnyngham@western.edu>
2014-04-28 15:40:40 -0600

    Update Difference.cs

--- VisualPOVRAY/VisualPOVRAY/Difference.cs
+++ VisualPOVRAY/VisualPOVRAY/Difference.cs
@@ -30,7 +30,7 @@ namespace VisualPOVRAY
          public List<string> render()
          {
              List<string> l = new List<string>();
-             l.Add("union {");
+             l.Add("difference {");
              l.AddRange(this.o1.render());
              foreach (PovObj o in o2)
              {
