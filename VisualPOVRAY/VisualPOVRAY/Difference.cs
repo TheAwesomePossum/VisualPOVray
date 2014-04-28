b932f0d219ad1e186a113db2569ff9400d91c130
westernState <bendix.cunnyngham@western.edu>
2014-04-28 13:52:16 -0600

    can take and array in the constructor now

--- VisualPOVRAY/VisualPOVRAY/Difference.cs
+++ VisualPOVRAY/VisualPOVRAY/Difference.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,34 +8,49 @@ namespace VisualPOVRAY
 {
     public class Difference:PovObj
     {
-         public PovObj o1, o2;
-         bool reactive;
+        public PovObj o1;
+        public PovObj[] o2;
+        bool reactive;
+
+         public Difference(PovObj o1, PovObj[] o2, bool reactive = false)
+         {
+             this.reactive = reactive;
+             this.o1 = o1;
+             this.o2 = o2;
+         }
 
          public Difference(PovObj o1, PovObj o2, bool reactive = false)
         {
             this.reactive = reactive;
             this.o1 = o1;
-            this.o2 = o2;   
+            this.o2 = new PovObj[1];
+            this.o2[0] = o2;
         }
 
-        public List<string> render()
-        {
-            List<string> l = new List<string>();
-            l.Add("difference {");
-            l.AddRange(this.o1.render());
-            l.AddRange(this.o2.render());
-            l.Add("}");
-            return l;
-        }
+         public List<string> render()
+         {
+             List<string> l = new List<string>();
+             l.Add("union {");
+             l.AddRange(this.o1.render());
+             foreach (PovObj o in o2)
+             {
+                 l.AddRange(o.render());
+             }
+             l.Add("}");
+             return l;
+         }
 
 
-        public void update(float time)
-        {
-            if (reactive)
-            {
-                o1.update(time);
-                o2.update(time);
-            }
-        }
+         public void update(float time)
+         {
+             if (reactive)
+             {
+                 o1.update(time);
+                 foreach (PovObj o in o2)
+                 {
+                     o.update(time);
+                 }
+             }
+         }
     }
 }
