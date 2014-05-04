f2ae55076d4174b1e6dc92b646cff7727b422d71
isaacgibbs <dirtbiker201@bresnan.net>
2014-05-04 16:43:57 -0600

    Update Sphere.cs
    Finish added

--- VisualPOVRAY/VisualPOVRAY/Sphere.cs
+++ VisualPOVRAY/VisualPOVRAY/Sphere.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,48 +8,51 @@ namespace VisualPOVRAY
 {
     class Sphere : PovObj
     {
-        public Point3 loc;
-        Signal<float> rad;
-        public Point3 trans;
-        public Point3 rot;
-        PovTexture tex;
+        public Point3 location;
+        Signal<float> radius;
+        public Point3 translate;
+        public Point3 rotation;
+        PovTexture texture;
         bool reactive;
+        String finish;
 
-        public Sphere(Point3 location = null, float radius = 1.0f, Signal<float> rrad = null, Point3 translate = null, Point3 rotation = null, PovTexture texture = null, bool reactive = false)
+        public Sphere(Point3 location = null, float radius = 1.0f, Signal<float> rrad = null, Point3 translate = null, Point3 rotation = null, PovTexture texture = null, String finish = null, bool reactive = false)
         {
             this.reactive = reactive;
-            this.loc = location ?? new Point3(0, 0, 0, reactive: reactive);
-            this.trans = translate ?? new Point3(0, 0, 0, reactive: reactive);
-            this.rot = rotation ?? new Point3(0, 0, 0, reactive: reactive);
-            this.tex = texture ?? new POVColor("Red");
-            this.rad = rrad ?? new Lift0f(radius);
+            this.location = location ?? new Point3(0, 0, 0, reactive: reactive);
+            this.translate = translate ?? new Point3(0, 0, 0, reactive: reactive);
+            this.rotation = rotation ?? new Point3(0, 0, 0, reactive: reactive);
+            this.texture = texture ?? new POVColor("Red");
+            this.finish = finish ?? "finish {phong .9 reflection .5}";
+            this.radius = rrad ?? new Lift0f(radius);
         }
 
         public List<string> render()
         {
             List<string> l = new List<string>();
             l.Add("sphere {");
-            l.Add("    " + this.loc.render()[0] + ", " + this.rad);
-            l.AddRange(this.tex.render());
-            l.Add("    rotate " + this.rot.render()[0]);
-            l.Add("    translate " + this.trans.render()[0]);
+            l.Add("    " + this.location.render()[0] + ", " + this.radius);
+            l.AddRange(this.texture.render());
+            l.Add("    rotate " + this.rotation.render()[0]);
+            l.Add("    translate " + this.translate.render()[0]);
+            l.Add(finish);
             l.Add("}");
             return l;
         }
 
         public void move(Point3 transform)
         {
-            this.trans = transform;
+            this.translate = transform;
         }
 
         public void update(float time)
         {
             if (reactive)
             {
-                this.loc.update(time);
-                this.trans.update(time);
-                this.rot.update(time);
-                this.rad.now(time);
+                this.location.update(time);
+                this.translate.update(time);
+                this.rotation.update(time);
+                this.radius.now(time);
             }
 
         }
