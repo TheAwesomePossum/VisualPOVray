7786caa5eeaf0af7c5b3d5db350d1e9cd4991a82
isaacgibbs <dirtbiker201@bresnan.net>
2014-05-04 16:41:05 -0600

    Update Cone.cs
    Reactive implemented

--- VisualPOVRAY/VisualPOVRAY/Cone.cs
+++ VisualPOVRAY/VisualPOVRAY/Cone.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,48 +8,61 @@ namespace VisualPOVRAY
 {
     class Cone : PovObj
     {
-        public Point3 botp;
-        public float botrad;
-        public Point3 topp;
-        public float toprad;
-        public Point3 trans;
-        public Point3 rot;
-        public PovTexture tex;
+        public Point3 bottompoint;
+        public Signal<float> bottomradius;
+        public Point3 toppoint;
+        public Signal<float> topradius;
+        public Point3 translate;
+        public Point3 rotation;
+        public PovTexture texture;
+        bool reactive;
+        String finish;
 
-        public Cone(Point3 bottompoint = null, float bottomradius = 1.0f, Point3 toppoint = null, float topradius = 0.0f, Point3 translate = null, Point3 rotation = null, PovTexture texture = null)
+        public Cone(Point3 bottompoint = null, float bottomradius = 1.0f, Signal<float> bottomradiusr = null, Point3 toppoint = null, float topradius = 0.0f, Signal<float> topradiusr = null, Point3 translate = null, Point3 rotation = null, PovTexture texture = null ,String finish = null, bool reactive = false)
         {
-            this.botp = bottompoint ?? new Point3(0, 0, 0);
-            this.botrad = bottomradius;
-            this.topp = toppoint ?? new Point3(0, 0, 0);
-            this.toprad = topradius;
-            this.trans = translate ?? new Point3(0, 0, 0);
-            this.rot = rotation ?? new Point3(0, 0, 0);
-            this.tex = texture ?? new POVColor("Red");
+            this.bottompoint = bottompoint ?? new Point3(0, 0, 0);
+            this.bottomradius = bottomradiusr ?? new Lift0f(bottomradius);
+            this.toppoint = toppoint ?? new Point3(0, 0, 0);
+            this.topradius = topradiusr ?? new Lift0f(topradius);
+            this.translate = translate ?? new Point3(0, 0, 0, reactive: reactive);
+            this.rotation = rotation ?? new Point3(0, 0, 0, reactive: reactive);
+            this.finish = finish ?? "finish {phong .9 reflection .5}";
+            this.texture = texture ?? new POVColor("Red");
+            this.reactive = reactive;
         }
 
         public List<string> render()
         {
             List<string> l = new List<string>();
             l.Add("cone {");
-            l.Add("    " + this.botp.render()[0] + ", " + botrad);
-            l.Add("    " + this.topp.render()[0] + ", " + toprad);
-            l.AddRange(this.tex.render());
-            l.Add("    rotate " + this.rot.render()[0]);
-            l.Add("    translate " + this.trans.render()[0]);
+            l.Add("    " + this.bottompoint.render()[0] + ", " + bottomradius);
+            l.Add("    " + this.toppoint.render()[0] + ", " + topradius);
+            l.AddRange(this.texture.render());
+            l.Add("    rotate " + this.rotation.render()[0]);
+            l.Add("    translate " + this.translate.render()[0]);
+            l.Add(finish);
             l.Add("}");
             return l;
         }
 
         public void move(Point3 transform)
         {
-            this.trans = transform;
+            this.translate = transform;
         }
 
 
 
         public void update(float time)
         {
-            throw new NotImplementedException();
+            if (reactive)
+            {
+                this.bottompoint.update(time);
+                this.bottomradius.now(time);
+                this.toppoint.update(time);
+                this.topradius.now(time);
+                this.translate.update(time);
+                this.rotation.update(time);
+            }
         }
     }
 }
